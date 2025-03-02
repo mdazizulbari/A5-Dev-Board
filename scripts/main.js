24ffdec00c244000975a1e221632b4056b0d0fee
smabari <mdazizulbarishafin@gmail.com>
2025-03-02 18:47:20 +0600

    random background color feature added

--- scripts/main.js
+++ scripts/main.js
@@ -94,3 +94,11 @@ document
     );
     logMessageContainer.innerHTML = ` `;
   });
+
+//   generate random background colors
+document.getElementById("theme-btn").addEventListener("click", function () {
+  const randomColor = Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, "0");
+  document.body.style.backgroundColor = `#${randomColor}`;
+});
